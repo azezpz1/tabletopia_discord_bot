4e6003d176ed1e2b3ef1ae27467d59b0c2d0efc7
Anthony Panettiere <azezpz1@gmail.com>
2020-05-04 22:22:12 -0500

    Create initial setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="tabletopia-discord-bot",
+    version="1.0",
+    description="A discord bot for picking tabletopia games",
+    license="MIT",
+    long_description=long_description,
+    author="Anthony Panettiere",
+    packages=["tabletopia"],  # same as name
+)
