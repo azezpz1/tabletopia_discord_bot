88f3c9eedd66c6bfd410ca1808c193499bfc44a6
Anthony Panettiere <azezpz1@gmail.com>
2020-05-17 18:14:33 -0500

    Change name in setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(
-    name="tabletopia-discord-bot",
+    name="tabletopia_discord_bot",
     version="1.0",
     description="A discord bot for picking tabletopia games",
     license="MIT",
     long_description=long_description,
     author="Anthony Panettiere",
-    packages=["tabletopia-discord-bot"],  # same as name
+    packages=["tabletopia_discord_bot"],  # same as name
 )
